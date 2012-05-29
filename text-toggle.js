d4f7ceaec0fc329cc89562056de1a8e16f19ad62
Jeremy Singer-Vine <jsvine@gmail.com>
2012-05-29 18:29:47 -0400

    Add header-comment and wrapper to js.

---
+++ text-toggle.js
@@ -0,0 +1,12 @@
+/*
+ * text-toggle
+ * https://github.com/jsvine/text-toggle
+ *
+ * Author: Jeremy Singer-Vine
+ * Based on a script originally published on Slate Magazine (slate.com)
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+}(jQuery));
